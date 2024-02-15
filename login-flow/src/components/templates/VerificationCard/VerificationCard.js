1e778b68f8d36fc68a81d0799a0f8b42d0f0313b
Romanpr0g <romankachanov3@gmail.com>
2024-02-15 15:33:33 +0300

    add verification card and server

--- login-flow/src/components/templates/VerificationCard/VerificationCard.js
+++ login-flow/src/components/templates/VerificationCard/VerificationCard.js
@@ -1,5 +1,68 @@
+import { ReactComponent as Arrow } from "./../../../assets/svg/arrow.svg";
+import "./style.css";
+import Welcome from "./../../molecules/Welcome/Welcome";
+import React, { useState } from "react";
+import OtpInput from "react-otp-input";
+
+import { NavLink, useLocation } from "react-router-dom";
+
 const VerificationCard = () => {
-  return <></>;
+  const [otp, setOtp] = useState("");
+  const { state } = useLocation();
+  const [loading, setLoading] = useState(false);
+  const { phoneNumber = "" } = state || {};
+  const [incorrect, setIncorrect] = useState(false);
+
+  const handleOtpChange = async (enteredOtp) => {
+    setIncorrect(false);
+    setOtp(enteredOtp);
+    if (enteredOtp.length === 5) {
+      setLoading(true);
+      const response = await fetch("/api/verify-otp", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ code: enteredOtp }),
+      }).then((response) => response.json());
+      setLoading(false);
+      if (response.success) {
+      } else {
+        setIncorrect(true);
+        setOtp("");
+      }
+    }
+  };
+
+  return (
+    <>
+      <NavLink to="/" className="btn--back">
+        <Arrow className="back__img" />
+        <span className="btn--back__text">Back</span>
+      </NavLink>
+      <Welcome />
+      <h3 className="subtitle">Verification code</h3>
+      <p className="telephone--info">
+        Please enter the verification code sent to number
+        <br />
+        <strong>{phoneNumber}</strong>
+      </p>
+      <div className="auth--block">
+        <OtpInput
+          inputStyle={`inputStyle ${incorrect ? "incorrect" : ""}`}
+          inputType="tel"
+          value={otp}
+          onChange={handleOtpChange}
+          placeholder="00000"
+          numInputs={5}
+          renderInput={(props) => <input {...props} disabled={loading} />}
+          shouldAutoFocus
+        />
+        {incorrect && <p className="auth--incorrect">Invalid verification code</p>}
+      </div>
+      <button className="btn--res">Resend code</button>
+    </>
+  );
 };
 
 export default VerificationCard;
--- login-flow/src/index.js
+++ login-flow/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { makeServer } from './server';
 
+makeServer();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
